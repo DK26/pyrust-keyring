bcc1b5984eb897b2a3cdb7ff361cd3f2a34700c3
David Krasnitsky <dikaveman@gmail.com>
2020-06-07 02:24:50 +0300

    Init -- Working Version

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use cpython::{Python, PyResult, PyObject, py_module_initializer, py_fn};
+
+py_module_initializer!(rskeyring, |py, m | {
+    m.add(py, "__doc__", "A keyring module imported from the Rust programming language. \n\
+    Rust Keyring Library(Crate): https://docs.rs/crate/keyring\n\
+    The motivation behind this conversion is to make an attempt to combine keyring with PyInstaller.")?;
+    m.add(py, "set_password", py_fn!(py, set_password(service: &str, username: &str, password: &str)))?;
+    m.add(py, "get_password", py_fn!(py, get_password(service: &str, username: &str)))?;
+    Ok(())
+});
+
+fn set_password(_py: Python, service: &str, username: &str, password: &str) -> PyResult<PyObject> {
+
+    let keyring_service = keyring::Keyring::new(service, username);
+    return match keyring_service.set_password(password) {
+        _ => Ok(_py.None())
+    }
+
+}
+
+fn get_password(_py: Python, service: &str, username: &str) -> PyResult<String> {
+
+    let keyring_service = keyring::Keyring::new(service, username);
+
+    return match keyring_service.get_password()
+    {
+
+        Ok(result) => Ok(result),
+        Err(_e) => Ok(String::new())
+    }
+
+}
