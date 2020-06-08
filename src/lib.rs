a368c78f527991d51f566622d4d0247454925fbb
David Krasnitsky <dikaveman@gmail.com>
2020-06-08 20:08:53 +0300

    Improved Python documentation by adding credit and more info.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
 use cpython::{Python, PyResult, PyObject, py_module_initializer, py_fn};
 
 py_module_initializer!(rskeyring, |py, m | {
-    m.add(py, "__doc__", "A keyring module imported from the Rust programming language. \n\
-    Rust Keyring Library(Crate): https://docs.rs/crate/keyring\n\
-    The main motivation behind this port is to enable keyring with PyInstaller.")?;
+
+    let doc =
+    r#"A C-level keyring module imported from the Rust programming language.
+
+        The main motivation behind creating this port was to enable keyring with PyInstaller.
+
+        Rust Keyring Library(Crate):
+            https://docs.rs/crate/keyring
+
+        GitHub Respositories:
+            Rust Keyring: https://github.com/hwchen/keyring-rs
+            Python Port: https://github.com/DK26/pyrust-keyring
+
+        Rust Keyring (keyring-rs) Copyright 2017 keyring developers
+
+        Python Port by David Krasnitsky <dikaveman@gmail.com>, <dave@cybersiem.com>
+            GitHub: https://github.com/DK26
+
+        Licenses:
+            Python Port: MIT
+            Rust Keyring (keyring-rs): MIT + Apache 2.0"#;
+
+    m.add(py, "__doc__", doc)?;
     m.add(py, "set_password", py_fn!(py, set_password(service: &str, username: &str, password: &str)))?;
     m.add(py, "get_password", py_fn!(py, get_password(service: &str, username: &str)))?;
     Ok(())
