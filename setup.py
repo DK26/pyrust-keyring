f44f782864df7591a5d6e559a1b7cef6cc2720e4
David Krasnitsky <dikaveman@gmail.com>
2020-06-28 22:46:40 +0300

    Added Multi-platform Support by using setuptools_rust for cross-platform compiling.

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
-import setuptools
+from setuptools import setup
+from setuptools import find_packages
+from setuptools_rust import Binding
+from setuptools_rust import RustExtension
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="rskeyring",
-    version="0.1.1",
+    version="0.1.2a0",
     author="David Krasnitsky",
     author_email="dikaveman@gmail.com",
     description="A C-level keyring module bind from the Rust programming language (crates.io)",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DK26/pyrust-keyring",
-    packages=setuptools.find_packages(),
+    rust_extensions=[RustExtension("rskeyring.rskeyring", binding=Binding.RustCPython)],
+    packages=["rskeyring"] + find_packages(),
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Rust",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent"
     ],
     python_requires='>=3.8',
+    zip_safe=False
 )
